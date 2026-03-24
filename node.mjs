84dd25c4b1dbb3968b62a2980922191c99bdcc4f
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-24 07:55:46 +0000

    Deploying to gh-pages from @ b-on-g/formigo@9f7f65b4dca25f4a2c692d23af29ad034de196a1 🚀

--- node.mjs
+++ node.mjs
@@ -16299,6 +16299,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
